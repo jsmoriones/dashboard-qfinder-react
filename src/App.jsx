583867b5b1c401d72d72d557087ca2f88e26b52f
juaness <sayayin4@gmail.com>
2025-03-21 10:14:56 -0500

    Modificamos el codigo para que le menu no sea visible en todo el sitio, solo en el dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Menu from "./components/Menu";
 import PerfilPaciente from "./pages/PerfilPaciente/PerfilPaciente";
@@ -13,27 +13,36 @@ import PerfilAcudiente from "./pages/PerfilAcudiente/PerfilAcudiente";
 import PerfilAuxiliar from "./pages/PerfilAuxiliar/PerfilAuxiliar";
 import Paciente from "./pages/PerfilPaciente/Paciente";
 
-function App() {
+function Layout(){
   return (
     <>
       <div className="bg-gradient-to-br from-azulTenueFuerte via-azulMasClaro to-azulMasClaro bg-no-repeat flex h-full min-h-screen">
         <Menu />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/perfil-paciente" element={<PerfilPaciente />} />
-          <Route path="/formulario-auxiliar" element={<FormularioAuxiliar />} />
-          <Route path="/turnos-auxiliar" element={<TurnosAuxiliar />} />
-          <Route path="/notas" element={<Notas />} />
-          <Route path="/configuracion" element={<Configuracion />} />
-          <Route path="/registro-notas" element={<RegistroNotas />} />
-          <Route path="/registro-paciente" element={<RegistrarPaciente />} />{/* Hanz A medias */}
-          <Route path="/perfil-acudiente" element={<PerfilAcudiente />} />
-          <Route path="/perfil-auxiliar" element={<PerfilAuxiliar />} />
-          <Route path="/pacientes" element={<Paciente />} />
+        <Outlet />
+      </div>
+    </>
+  )
+}
 
+function App() {
+  return (
+    <>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/perfil-paciente" element={<PerfilPaciente />} />
+            <Route path="/formulario-auxiliar" element={<FormularioAuxiliar />} />
+            <Route path="/turnos-auxiliar" element={<TurnosAuxiliar />} />
+            <Route path="/notas" element={<Notas />} />
+            <Route path="/configuracion" element={<Configuracion />} />
+            <Route path="/registro-notas" element={<RegistroNotas />} />
+            <Route path="/registro-paciente" element={<RegistrarPaciente />} />
+            <Route path="/perfil-acudiente" element={<PerfilAcudiente />} />
+            <Route path="/perfil-auxiliar" element={<PerfilAuxiliar />} />
+            <Route path="/pacientes" element={<Paciente />} />
+          </Route>
           <Route path="/login" element={<Login />} />
         </Routes>
-      </div>
     </>
   )
 }
